32f27fa26957fcbbe463a640d1d300b239e17386
qusai-alhanaktah <qusaialhanaktah@gmail.com>
2019-11-06 00:41:01 +0200

    javascript effects

---
+++ js/content.js
@@ -0,0 +1,21 @@
+//var today = new Date();
+//var hournow = today.getHours();
+var user = prompt("what is your user?")
+var moneys = prompt("how many money do you have");
+
+if (moneys > 10000) {
+    car = "I advise you to buy Mercedes";   
+} else if (moneys < 10000 && money > 5000) {
+
+        car = "I advise you to buy Toyta"; 
+} 
+        else {
+            car = "I advise you to buy kia sevia 1";    
+              }
+
+
+
+document .write('<h3>' + user + '</h3>'); 
+document .write('<h3>' + car + '</h3>'); 
+
+
